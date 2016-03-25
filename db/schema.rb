4ec1921e985436ee8a4d0b5b6f58827e3d74f811
Yamil Jalil Maluf <yamil.jalil@despegar.com>
2016-03-25 01:31:05 -0300

    Add postgre

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160324234314) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "field_definitions", force: true do |t|
     t.string   "name"
     t.string   "field_type"
