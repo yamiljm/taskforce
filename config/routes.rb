5950ee5abb0bfb199e12d8d804d8b82496cd9fe4
Yamil Jalil Maluf <yamil.jalil@despegar.com>
2016-11-16 17:04:49 -0300

    Agrego Step scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :steps
+
   resources :task_definitions
 
   resources :users do
---
+++ db/migrate/20161116200342_create_steps.rb
@@ -0,0 +1,10 @@
+class CreateSteps < ActiveRecord::Migration
+  def change
+    create_table :steps do |t|
+      t.integer :order
+      t.references :task, index: true
+
+      t.timestamps
+    end
+  end
+end
