239ef76ed2acaa0ef0c9bfbdff208446e8df72c9
Yamil Jalil Maluf <yamil.jalil@despegar.com>
2016-11-21 01:19:58 -0300

    Borro migraciones que no van

--- db/migrate/20161117155531_add_task_to_step.rb
+++
@@ -1,5 +0,0 @@
-class AddTaskToStep < ActiveRecord::Migration
-  def change
-    add_column :steps, :task, :references
-  end
-end
--- db/migrate/20161117155752_add_step_to_field.rb
+++
@@ -1,5 +0,0 @@
-class AddStepToField < ActiveRecord::Migration
-  def change
-    add_column :fields, :step, :reference
-  end
-end
